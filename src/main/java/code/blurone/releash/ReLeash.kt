03d36f4b7002a24d2b72c921c0f0774ae8f9d11d
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-06-06 17:06:26 -0300

    build: create project

---
+++ src/main/java/code/blurone/releash/ReLeash.kt
@@ -0,0 +1,14 @@
+package code.blurone.releash
+
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("unused")
+class ReLeash : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
